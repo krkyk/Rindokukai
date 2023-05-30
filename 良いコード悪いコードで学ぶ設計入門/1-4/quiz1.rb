8c010b5664f8ba3f5f7d0bb1cff9e87866900478
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-05-30 20:03:36 +0900

    エラー無いように変更

--- 良いコード悪いコードで学ぶ設計入門/1-4/quiz1.rb
+++ 良いコード悪いコードで学ぶ設計入門/1-4/quiz1.rb
@@ -8,7 +8,7 @@ class Monster
 end
 
 class HitPoint
-  attr_accessor :hitpoint
+  attr_accessor :point
 
   def initialize
     @point = 100
@@ -16,8 +16,8 @@ class HitPoint
 end
 
 # Monster を HitPoint を使って初期化する例
-hitpoint = HitPoint.new(100)
+hitpoint = HitPoint.new
 weak_monster = Monster.new("Goblin", hitpoint.point)
 strong_monster = Monster.new("Gliffon", hitpoint.point)
 
-puts strong_monster
+puts strong_monster.hitpoint
